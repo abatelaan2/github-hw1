b1067e4e54143492d695e216391b5730be8e0bfe
abatelaan2 <abatelaan@hotmail.com>
2020-06-09 23:24:46 -0500

    test change

--- 3dcolorfunction.py
+++ 3dcolorfunction.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import mpl_toolkits.mplot3d.art3d as art3d
@@ -8,39 +9,20 @@ import matplotlib as mpl
 empty = {}
 df = pd.DataFrame(empty)
 
-step=0.1
-x_list=[]
-y_list=[]
-z_list=[]
-n=-1
-while n<=1:
-    x = n
-    m=-1
-    while m<=1:
-        y = m
-        l=-1
-        while l<=1:
-            z = l
-            x_list.append(x)
-            y_list.append(y)
-            z_list.append(z)
-            l+=step
-        m+=step
-    n+=step
-
-df["x"]=x_list
-df["y"]=y_list
-df["z"]=z_list
+number= 10000
+df["x"]= np.random.uniform(low=-1., high=1., size=(number,))
+df["y"]= np.random.uniform(low=-1., high=1., size=(number,))
+df["z"]= np.random.uniform(low=-1., high=1., size=(number,))
 
 color_list=[]
 n=0
 while n<=len(df["x"])-1:
     function = df["x"][n]**2+df["y"][n]**2+df["z"][n]**2
-    if function < 0:
-        function = 0
+    if function < 0.:
+        function = 0.
         color_list.append(function)
-    elif function > 1:
-        function = 1
+    elif function > 1.:
+        function = 1.
         color_list.append(function)
     else:
         color_list.append(function)
